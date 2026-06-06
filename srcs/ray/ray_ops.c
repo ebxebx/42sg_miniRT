881e1bec9dd359d2570fd32d35273f856952d1c1
Zeon <zchoo@student.42singapore.sg>
2026-06-06 19:05:25 +0800

    Refactor hit_sphere function to return intersection distance and update ray_color logic

--- srcs/ray/ray_ops.c
+++ srcs/ray/ray_ops.c
@@ -6,7 +6,7 @@
 /*   By: zchoo <zchoo@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 20:43:45 by zchoo             #+#    #+#             */
-/*   Updated: 2026/06/06 18:55:44 by zchoo            ###   ########.fr       */
+/*   Updated: 2026/06/06 19:04:35 by zchoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,34 @@ double lerp(double start, double end, double t)
     return (start + t * (end - start));
 }
 
-int hit_sphere(const t_vec3 center, double radius, const t_ray r) {
+double hit_sphere(const t_vec3 center, double radius, const t_ray r) {
     t_vec3 oc = vec3_sub(r.origin, center);
     double a = vec3_dot(r.direction, r.direction);
     double b = 2.0 * vec3_dot(oc, r.direction);
     double c = vec3_dot(oc, oc) - radius*radius;
     double discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+
+    if (discriminant < 0)
+        return -1.0;
+	 else
+        return (-b - sqrt(discriminant)) / (2.0*a);
+}
+
+t_vec3 ray_at(t_ray r, double t)
+{
+	return vec3_add(r.origin, vec3_scale(r.direction, t));
 }
 
 t_color	ray_color(t_ray r)
 {
-    if (hit_sphere(vec3_init(0, 0, -1.0), 0.5, r)) {
-        return (t_color){1.0, 0, 0};
+	double t = hit_sphere(vec3_init(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        t_vec3 N = vec3_norm(vec3_sub(ray_at(r, t), vec3_init(0, 0, -1)));
+        return (t_color){
+			.r =  0.5 * (N.x + 1),
+			.g =  0.5 * (N.y + 1),
+			.b =  0.5 * (N.z + 1)
+		};
     }
 	
     t_vec3 unit_direction = vec3_norm(r.direction);
